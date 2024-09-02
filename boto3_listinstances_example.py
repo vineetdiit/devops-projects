fbb6d87fc307a8f8add80063caabca668de07897
VinRoc <vineetdiit@gmail.com>
2024-09-02 11:13:03 +0530

    Create boto3_listinstances_example.py
    added boto3 list ec2 instances

---
+++ boto3_listinstances_example.py
@@ -0,0 +1,23 @@
+import boto3
+
+ec2 = boto3.resource('ec2')
+
+def lambda_handler(event, context):
+    # create filter for instances in running state
+    filters = [
+        {
+            'Name': 'instance-state-name', 
+            'Values': ['running']
+        }
+    ]
+    
+    # filter the instances based on filters() above
+    instances = ec2.instances.filter(Filters=filters)
+
+    # instantiate empty array
+    RunningInstances = []
+
+    for instance in instances:
+        # for each instance, append to array and print instance id
+        RunningInstances.append(instance.id)
+        print instance.id
